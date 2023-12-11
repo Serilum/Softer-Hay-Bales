d1963b6e6f911589191db77c38b646151a14bf9a
Rick South <rick@natamus.com>
2023-12-12 00:04:25 +0100

    Softer Hay Bales source code for 1.20.0.

---
+++ Fabric/src/main/java/com/natamus/softerhaybales/ModFabric.java
@@ -0,0 +1,32 @@
+package com.natamus.softerhaybales;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.fabric.callbacks.CollectiveEntityEvents;
+import com.natamus.softerhaybales.events.FallEvent;
+import com.natamus.softerhaybales.util.Reference;
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
+
+public class ModFabric implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		setGlobalConstants();
+		ModCommon.init();
+
+		loadEvents();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadEvents() {
+		CollectiveEntityEvents.ON_FALL_DAMAGE_CALC.register((Level level, Entity entity, float f, float g) -> {
+			return FallEvent.onFall(level, entity, f, g);
+		});
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
