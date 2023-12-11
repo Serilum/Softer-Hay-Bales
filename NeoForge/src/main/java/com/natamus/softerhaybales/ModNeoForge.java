683de9bbd3897404c01e895a7e3b3793ae1defc2
Rick South <rick@natamus.com>
2023-12-12 00:04:34 +0100

    Softer Hay Bales source code for 1.20.2.

---
+++ NeoForge/src/main/java/com/natamus/softerhaybales/ModNeoForge.java
@@ -0,0 +1,32 @@
+package com.natamus.softerhaybales;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.softerhaybales.neoforge.events.NeoForgeFallEvent;
+import com.natamus.softerhaybales.util.Reference;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(Reference.MOD_ID)
+public class ModNeoForge {
+	
+	public ModNeoForge() {
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		modEventBus.addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+		NeoForge.EVENT_BUS.register(NeoForgeFallEvent.class);
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
