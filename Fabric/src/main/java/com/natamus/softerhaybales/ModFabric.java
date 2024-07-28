b5655153d55a9a00c91957761f33320ad01d484c
Rick South <rick@natamus.com>
2024-07-28 07:14:15 +0200

    (v3.3) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Fabric/src/main/java/com/natamus/softerhaybales/ModFabric.java
+++ Fabric/src/main/java/com/natamus/softerhaybales/ModFabric.java
@@ -1,6 +1,7 @@
 package com.natamus.softerhaybales;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.collective.fabric.callbacks.CollectiveEntityEvents;
 import com.natamus.softerhaybales.events.FallEvent;
 import com.natamus.softerhaybales.util.Reference;
@@ -12,6 +13,10 @@ public class ModFabric implements ModInitializer {
 	
 	@Override
 	public void onInitialize() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		setGlobalConstants();
 		ModCommon.init();
 
