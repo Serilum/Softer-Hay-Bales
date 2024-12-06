270304f6b262f0532332e022a3901800c44c09d2
Rick South <rick@natamus.com>
2024-12-06 13:12:30 +0100

    Softer Hay Bales source code for 1.21.4.

---
+++ Forge/src/main/java/com/natamus/softerhaybales/forge/events/ForgeFallEvent.java
@@ -0,0 +1,18 @@
+package com.natamus.softerhaybales.forge.events;
+
+import com.natamus.softerhaybales.events.FallEvent;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraftforge.event.entity.living.LivingFallEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class ForgeFallEvent {
+	@SubscribeEvent
+	public void onFall(LivingFallEvent e) {
+		LivingEntity livingEntity = e.getEntity();
+		if (FallEvent.onFall(livingEntity.level(), livingEntity, 0, 0) == 0) {
+			e.setCanceled(true);
+		}
+	}
+}
---
+++ NeoForge/src/main/java/com/natamus/softerhaybales/neoforge/events/NeoForgeFallEvent.java
@@ -0,0 +1,18 @@
+package com.natamus.softerhaybales.neoforge.events;
+
+import com.natamus.softerhaybales.events.FallEvent;
+import net.minecraft.world.entity.LivingEntity;
+import net.neoforged.neoforge.event.entity.living.LivingFallEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeFallEvent {
+	@SubscribeEvent
+	public static void onFall(LivingFallEvent e) {
+		LivingEntity livingEntity = e.getEntity();
+		if (FallEvent.onFall(livingEntity.level(), livingEntity, 0, 0) == 0) {
+			e.setCanceled(true);
+		}
+	}
+}
