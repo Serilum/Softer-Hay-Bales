a765a43c66c26107161805f6112b9d187aee7ca8
Rick South <rick@natamus.com>
2025-02-12 23:12:46 +0100

    (v3.4) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Forge/src/main/java/com/natamus/softerhaybales/forge/events/ForgeFallEvent.java
+++ Forge/src/main/java/com/natamus/softerhaybales/forge/events/ForgeFallEvent.java
@@ -4,12 +4,10 @@ import com.natamus.softerhaybales.events.FallEvent;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraftforge.event.entity.living.LivingFallEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
-@EventBusSubscriber
 public class ForgeFallEvent {
 	@SubscribeEvent
-	public void onFall(LivingFallEvent e) {
+	public static void onFall(LivingFallEvent e) {
 		LivingEntity livingEntity = e.getEntity();
 		if (FallEvent.onFall(livingEntity.level(), livingEntity, 0, 0) == 0) {
 			e.setCanceled(true);
