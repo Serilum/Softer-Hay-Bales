fdfd845fd5b2cb1810df22bbbbfb9543db5ea5ca
Rick South <rick@natamus.com>
2025-12-11 02:31:26 +0100

    Softer Hay Bales source code for 1.21.11.

---
+++ NeoForge/src/main/java/com/natamus/softerhaybales/neoforge/events/NeoForgeFallEvent.java
@@ -0,0 +1,16 @@
+package com.natamus.softerhaybales.neoforge.events;
+
+import com.natamus.softerhaybales.events.FallEvent;
+import net.minecraft.world.entity.LivingEntity;
+import net.neoforged.neoforge.event.entity.living.LivingFallEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+
+public class NeoForgeFallEvent {
+	@SubscribeEvent
+	public static void onFall(LivingFallEvent e) {
+		LivingEntity livingEntity = e.getEntity();
+		if (FallEvent.onFall(livingEntity.level(), livingEntity, 0, 0) == 0) {
+			e.setCanceled(true);
+		}
+	}
+}
