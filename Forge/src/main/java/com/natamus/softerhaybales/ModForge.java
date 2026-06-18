e74702ef3166185e0d89897b715604fc82423efa
Rick South <rick@natamus.com>
2026-06-18 22:23:41 +0200

    Updated Softer Hay Bales to Minecraft version 26.2.0.

--- Forge/src/main/java/com/natamus/softerhaybales/ModForge.java
+++ Forge/src/main/java/com/natamus/softerhaybales/ModForge.java
@@ -4,7 +4,6 @@ import com.natamus.collective.check.RegisterMod;
 import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.softerhaybales.forge.events.ForgeFallEvent;
 import com.natamus.softerhaybales.util.Reference;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.bus.BusGroup;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
